8819177b5d06d769b0f7deae37f6e5a990c5fc7c
ASHISH VERMA <ashishkv845@gmail.com>
2023-06-14 23:24:01 +0530

    buils

--- src/component/ShowItem/ShowMeal.jsx
+++ src/component/ShowItem/ShowMeal.jsx
@@ -26,27 +26,9 @@ const ShowMeal = () => {
    
    
   return (
-    <div >
+    <div npm buils>
         <ItemList data={categoryItem} name={param.item}/>
-        {/* <h3 onClick={()=>navigate('/')}>Back </h3>
-        <div className='imgContainer'>
-            <div className='mainImg'>
-                <img src="https://www.themealdb.com/images/media/meals/va668f1683209318.jpg" alt="" />
-            </div>
-           <div className='mealItemList'>
-           {
-                categoryItem?.map((item=>(
-                        <div className='mealItem' onClick={()=>navigate(`/${param.item}/${item?.idMeal}`)}>
-                            <img src={item?.strMealThumb} alt={item.strMeal} />
-                            <h4>{item.strMeal}</h4>
-                            
-                        </div>
-
-
-                )))
-            }
-           </div>
-        </div> */}
+        
     </div>
   )
 }
